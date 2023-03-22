ef9bf32f880ff19ff267be384fd711659997ecc2
洪靖睿 <s1101711@gm.pu.edu.tw>
2023-03-22 10:17:13 +0800

    第一次 Commit Message

---
+++ app/src/main/java/tw/edu/pu/gm/s1101711/counter/MainActivity.kt
@@ -0,0 +1,11 @@
+package tw.edu.pu.gm.s1101711.counter
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
